2279e9ccf561d051474ef21daf18b8e80f50da15
Joaquin <jokinve1997@gmail.com>
2025-06-17 23:52:12 -0300

    Cambios en el environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //apiUrl: 'http://localhost:3000'
-  apiUrl: 'https://ttads-backend-production-455e.up.railway.app',
+  //apiUrl: 'https://ttads-backend-production-455e.up.railway.app',
+  apiUrl: 'https://ttads-backend-production-b0f8.up.railway.app'
 }
